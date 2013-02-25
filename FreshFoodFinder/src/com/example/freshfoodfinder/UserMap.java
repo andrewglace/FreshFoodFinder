cd9c29eb3c0b17a92c835631fe7136582ee688cc
Andrew Glace <your_email@youremail.com>
2013-02-24 22:09:42 -0500

    Changed Usermap.java to get user location from GPS and display location

--- FreshFoodFinder/src/com/example/freshfoodfinder/UserMap.java
+++ FreshFoodFinder/src/com/example/freshfoodfinder/UserMap.java
@@ -1,19 +1,35 @@
 package com.example.freshfoodfinder;
 
+import java.security.Provider;
+
 import android.app.Activity;
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.Menu;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.GoogleMapOptions;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.maps.GeoPoint;
 
 public class UserMap extends Activity {
 
-	private GoogleMap mMap;
+	private GoogleMap map;
 	private MapFragment mapFragment;
 	private GeoPoint geo;
+	private LocationManager locationManager;
+	private String provider;
+	
+	static final LatLng HAMBURG = new LatLng(53.558, 9.927);
+	 static final LatLng KIEL = new LatLng(53.551, 9.993);
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -21,13 +37,41 @@ public class UserMap extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.usermap);
 		mapFragment = ((MapFragment) getFragmentManager().findFragmentById(R.id.map));
-		mMap = mapFragment.getMap();
+		map = mapFragment.getMap();
 		setUpMapIfNeeded();
 		GoogleMapOptions options = new GoogleMapOptions();
 		options.mapType(GoogleMap.MAP_TYPE_NORMAL)
 	    .compassEnabled(false)
 	    .rotateGesturesEnabled(false)
 	    .tiltGesturesEnabled(false);
+		
+		map.setMyLocationEnabled(true);
+		
+		locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+	    // Define the criteria how to select the locatioin provider -> use
+	    // default
+	    Criteria criteria = new Criteria();
+	    provider = locationManager.getBestProvider(criteria, false);
+	    Location location = locationManager.getLastKnownLocation(provider);
+	    
+	    LatLng userLatLng = new LatLng(location.getLatitude(),location.getLongitude());
+		
+		Marker hamburg = map.addMarker(new MarkerOptions().position(HAMBURG)
+		        .title("Hamburg"));
+		    Marker kiel = map.addMarker(new MarkerOptions()
+		        .position(KIEL)
+		        .title("Kiel")
+		        .snippet("Kiel is cool")
+		        .icon(BitmapDescriptorFactory
+		            .fromResource(R.drawable.ic_launcher)));
+
+		    // Move the camera instantly to user's location with a zoom of 15.
+		    map.moveCamera(CameraUpdateFactory.newLatLngZoom(userLatLng, 30));
+
+		    // Zoom in, animating the camera.
+		    map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+		    
+		    
 
 	}
 
@@ -44,11 +88,11 @@ public class UserMap extends Activity {
 	 */
 	private void setUpMapIfNeeded() {
 	    // Do a null check to confirm that we have not already instantiated the map.
-	    if (mMap == null) {
-	        mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
+	    if (map == null) {
+	        map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
 	                            .getMap();
 	        // Check if we were successful in obtaining the map.
-	        if (mMap != null) {
+	        if (map != null) {
 	            // The Map is verified. It is now safe to manipulate the map.
 
 	        }
