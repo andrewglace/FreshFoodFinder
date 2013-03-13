37babbfb7a58b9a17d7f239650b72cc4025da844
Nebil Ali <nebilali@seas644.wireless-pennnet.upenn.edu>
2013-03-13 18:17:59 -0400

    Added vegtables

--- FreshFoodFinder/src/com/example/freshfoodfinder/MainActivity.java
+++ FreshFoodFinder/src/com/example/freshfoodfinder/MainActivity.java
@@ -32,11 +32,65 @@ public class MainActivity extends ListActivity {
 		
 		
 		//-----veggies
+		allFoods.add(new Food("Arugula", Food.FOOD_VEG, Calendar.JANUARY, Calendar.DECEMBER));
+		allFoods.add(new Food("Beets", Food.FOOD_VEG, Calendar.JANUARY, Calendar.DECEMBER));
+		allFoods.add(new Food("Broccoli", Food.FOOD_VEG, Calendar.JANUARY, Calendar.DECEMBER));
+		allFoods.add(new Food("Brussels sprouts", Food.FOOD_VEG, Calendar.JANUARY, Calendar.DECEMBER));
+		allFoods.add(new Food("Cabbage", Food.FOOD_VEG, Calendar.JANUARY, Calendar.DECEMBER));
+		allFoods.add(new Food("Celery", Food.FOOD_VEG, Calendar.JANUARY, Calendar.DECEMBER));
+		allFoods.add(new Food("Collard Greens", Food.FOOD_VEG, Calendar.JANUARY, Calendar.DECEMBER));
+		allFoods.add(new Food("Kale", Food.FOOD_VEG, Calendar.JANUARY, Calendar.DECEMBER));
+		allFoods.add(new Food("Lettuce", Food.FOOD_VEG, Calendar.JANUARY, Calendar.DECEMBER));
+		allFoods.add(new Food("Mustard", Food.FOOD_VEG, Calendar.JANUARY, Calendar.DECEMBER));
+		allFoods.add(new Food("Peas", Food.FOOD_VEG, Calendar.JANUARY, Calendar.DECEMBER));
+		allFoods.add(new Food("Spinach", Food.FOOD_VEG, Calendar.JANUARY, Calendar.DECEMBER));
+		allFoods.add(new Food("Swiss Chard", Food.FOOD_VEG, Calendar.JANUARY, Calendar.DECEMBER));
+		allFoods.add(new Food("Turnip greens", Food.FOOD_VEG, Calendar.JANUARY, Calendar.DECEMBER));
+		allFoods.add(new Food("Bell pepper", Food.FOOD_VEG, Calendar.JANUARY, Calendar.DECEMBER));
+		allFoods.add(new Food("Cucumber", Food.FOOD_VEG, Calendar.JANUARY, Calendar.DECEMBER));
+		allFoods.add(new Food("Eggplant", Food.FOOD_VEG, Calendar.JANUARY, Calendar.DECEMBER));
+		allFoods.add(new Food("Pumpkin", Food.FOOD_VEG, Calendar.JANUARY, Calendar.DECEMBER));
+		allFoods.add(new Food("Squash", Food.FOOD_VEG, Calendar.JANUARY, Calendar.DECEMBER));
+		allFoods.add(new Food("Sweet corn", Food.FOOD_VEG, Calendar.JANUARY, Calendar.DECEMBER));
+		allFoods.add(new Food("Sweet pepper", Food.FOOD_VEG, Calendar.JANUARY, Calendar.DECEMBER));
+		allFoods.add(new Food("Tomato", Food.FOOD_VEG, Calendar.JANUARY, Calendar.DECEMBER));
+		allFoods.add(new Food("Zucchini", Food.FOOD_VEG, Calendar.JANUARY, Calendar.DECEMBER));
+		allFoods.add(new Food("Artichoke", Food.FOOD_VEG, Calendar.JANUARY, Calendar.DECEMBER));
+		allFoods.add(new Food("Cauliflower", Food.FOOD_VEG, Calendar.JANUARY, Calendar.DECEMBER));
+		allFoods.add(new Food("Black-eyed peas", Food.FOOD_VEG, Calendar.JANUARY, Calendar.DECEMBER));
+		allFoods.add(new Food("Garbanzo", Food.FOOD_VEG, Calendar.JANUARY, Calendar.DECEMBER));
+		allFoods.add(new Food("Green bean", Food.FOOD_VEG, Calendar.JANUARY, Calendar.DECEMBER));
+		allFoods.add(new Food("Lentil", Food.FOOD_VEG, Calendar.JANUARY, Calendar.DECEMBER));
+		allFoods.add(new Food("Lima Bean", Food.FOOD_VEG, Calendar.JANUARY, Calendar.DECEMBER));
+		allFoods.add(new Food("Okra", Food.FOOD_VEG, Calendar.JANUARY, Calendar.DECEMBER));
+		allFoods.add(new Food("Snap pea", Food.FOOD_VEG, Calendar.JANUARY, Calendar.DECEMBER));
+		allFoods.add(new Food("Snow pea", Food.FOOD_VEG, Calendar.JANUARY, Calendar.DECEMBER));
+		allFoods.add(new Food("Soybean", Food.FOOD_VEG, Calendar.JANUARY, Calendar.DECEMBER));
+		allFoods.add(new Food("Asparagus", Food.FOOD_VEG, Calendar.JANUARY, Calendar.DECEMBER));
+		allFoods.add(new Food("Florence fennel", Food.FOOD_VEG, Calendar.JANUARY, Calendar.DECEMBER));
+		allFoods.add(new Food("Garlic", Food.FOOD_VEG, Calendar.JANUARY, Calendar.DECEMBER));
+		allFoods.add(new Food("Leek", Food.FOOD_VEG, Calendar.JANUARY, Calendar.DECEMBER));
+		allFoods.add(new Food("Onion", Food.FOOD_VEG, Calendar.JANUARY, Calendar.DECEMBER));
+		allFoods.add(new Food("Scallion", Food.FOOD_VEG, Calendar.JANUARY, Calendar.DECEMBER));
+		allFoods.add(new Food("Shallot", Food.FOOD_VEG, Calendar.JANUARY, Calendar.DECEMBER));
+		allFoods.add(new Food("Beetroot", Food.FOOD_VEG, Calendar.JANUARY, Calendar.DECEMBER));
+		allFoods.add(new Food("Carrot", Food.FOOD_VEG, Calendar.JANUARY, Calendar.DECEMBER));
+		allFoods.add(new Food("Daikon", Food.FOOD_VEG, Calendar.JANUARY, Calendar.DECEMBER));
+		allFoods.add(new Food("Ginger", Food.FOOD_VEG, Calendar.JANUARY, Calendar.DECEMBER));
+		allFoods.add(new Food("Parsnip", Food.FOOD_VEG, Calendar.JANUARY, Calendar.DECEMBER));
+		allFoods.add(new Food("Potato", Food.FOOD_VEG, Calendar.JANUARY, Calendar.DECEMBER));
+		allFoods.add(new Food("Radish", Food.FOOD_VEG, Calendar.JANUARY, Calendar.DECEMBER));
+		allFoods.add(new Food("Sweet potato", Food.FOOD_VEG, Calendar.JANUARY, Calendar.DECEMBER));
+		allFoods.add(new Food("Turnip", Food.FOOD_VEG, Calendar.JANUARY, Calendar.DECEMBER));
+		allFoods.add(new Food("Yam", Food.FOOD_VEG, Calendar.JANUARY, Calendar.DECEMBER));
 		
 		
 		
-
-		    ArrayAdapter<Food> adapter = new ArrayAdapter<Food>(this,
+		
+		
+		
+		
+		ArrayAdapter<Food> adapter = new ArrayAdapter<Food>(this,
 		        android.R.layout.simple_list_item_1, allFoods);
 		    setListAdapter(adapter);		
 	}
