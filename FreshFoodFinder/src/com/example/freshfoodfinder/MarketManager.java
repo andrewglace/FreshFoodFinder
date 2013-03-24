6b290892ea3d1f3b79bc07029681362c217dfa5c
Andrew Glace <your_email@youremail.com>
2013-03-24 13:41:59 -0400

    Added MarketManager Class that will do market handling in UserMap

---
+++ FreshFoodFinder/src/com/example/freshfoodfinder/MarketManager.java
@@ -0,0 +1,35 @@
+package com.example.freshfoodfinder;
+
+import java.util.ArrayList;
+
+public class MarketManager {
+
+	private ArrayList<Food> superMarketFoods;
+	private ArrayList<Food> wawaFoods;
+	private ArrayList<Food> cornerStoreFoods;
+	private ArrayList<Food> farmerMarketFoods;
+	private ArrayList<Food> aldiFoods;
+	
+	private ArrayList<Market> superMarkets;
+	private ArrayList<Market> wawas;
+	private ArrayList<Market> cornerStores;
+	private ArrayList<Market> farmerMarkets;
+	
+	public MarketManager() {
+		superMarketFoods = new ArrayList<Food>();
+		wawaFoods = new ArrayList<Food>();
+		cornerStoreFoods = new ArrayList<Food>();
+		farmerMarketFoods = new ArrayList<Food>();
+		aldiFoods = new ArrayList<Food>();
+		
+
+	}
+	
+	private void instantiateSuperMarketFoodList() {
+
+	}
+	
+	private void instantiateWawaFoods() {
+		
+	}
+}
